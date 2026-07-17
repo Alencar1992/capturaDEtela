f218184a0d7cf6074a6eaa931d4e94d9033e1f5d
LUCAS ALENCAR <lucas.aluncar.l70@gmail.com>
2026-07-17 15:31:56 -0300

    feat: support single-key shortcuts and dated saves

--- desktop/CapturaRapida.Native/AppConfig.cs
+++ desktop/CapturaRapida.Native/AppConfig.cs
@@ -3,10 +3,10 @@ namespace CapturaRapida.Native;
 internal sealed class AppConfig
 {
     public bool SetupCompleted { get; set; }
-    public HotkeyModifiers Modifiers { get; set; } = HotkeyModifiers.Control | HotkeyModifiers.Alt;
-    public Keys Key { get; set; } = Keys.P;
-    public HotkeyModifiers SelectionModifiers { get; set; } = HotkeyModifiers.Control | HotkeyModifiers.Alt;
-    public Keys SelectionKey { get; set; } = Keys.S;
+    public HotkeyModifiers Modifiers { get; set; } = HotkeyModifiers.None;
+    public Keys Key { get; set; } = Keys.F8;
+    public HotkeyModifiers SelectionModifiers { get; set; } = HotkeyModifiers.None;
+    public Keys SelectionKey { get; set; } = Keys.F9;
     public bool StartWithWindows { get; set; }
     public bool SaveToFile { get; set; }
     public string SaveDirectory { get; set; } = Path.Combine(
